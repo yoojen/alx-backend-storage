4e3af2dbf831050daeeae43e59e7b16198c67219
yoojen <eugeneemma7@gmail.com>
2023-12-19 07:18:10 -0800

    extracted information from imported db

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+    Database: logs
+    Collection: nginx
+    Display (same as the example):
+        first line: x logs where x is the number of documents
+        in this collection
+        second line: Methods:
+        5 lines with the number of documents with the method = ["GET", "POST"
+        , "PUT", "PATCH", "DELETE"] in this order (see example below - warning:
+        it’s a tabulation before each line)
+        one line with the number of documents with:
+            method=GET
+            path=/status
+"""
+
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = client.logs
+    print(f"{client.nginx.count_documents({})} logs")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print('Methods:')
+    for method in methods:
+        count = client.nginx.count_documents({'method': method})
+        print(f"\tmethod {method} : {count}")
+    status = client.nginx.count_documents({'method': 'GET', 'path': '/status'})
+    print(f'{status} status check')
