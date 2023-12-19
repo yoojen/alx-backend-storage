eb487914b50a322cad98a040818139e2d4072228
Eugene <140322568+yoojen@users.noreply.github.com>
2023-12-19 17:41:07 +0200

    Update 9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+    """Insert new data in school collection"""
 
 
 def insert_school(mongo_collection, **kwargs):
     """Insert new data in school collection"""
-    return mongo_collection.insert(kwargs)
+    return mongo_collection.insert_one(kwargs)
