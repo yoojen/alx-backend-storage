9b35a5ceba2b70fde63272c8767cf5531efa5467
Eugene <140322568+yoojen@users.noreply.github.com>
2023-12-19 17:45:38 +0200

    replace one with many

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,4 +4,4 @@
 
 def update_topics(mongo_collection, name, topics):
     """update documents in collection"""
-    mongo_collection.update_one({"name": name}, {'$set': {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {'$set': {"topics": topics}})
