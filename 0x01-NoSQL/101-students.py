1180c9467f6ba93f42acc426ead39ea260900bef
Eugene <140322568+yoojen@users.noreply.github.com>
2023-12-19 18:58:48 +0200

    Update 101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """returns sorted list of students according to average scores"""
-from pymongo import MongoClient
 
 
 def top_students(mongo_collection):
     """returns sorted list of students according to average scores"""
     return mongo_collection.aggregate([
         {"$project": {
-            "_id": "$name", "averageScore": {"$avg": "$topics.score"}
+            "name": "$name", "averageScore": {"$avg": "$topics.score"}
         }},
         {"$sort": {"averageScore": -1}}
     ])
