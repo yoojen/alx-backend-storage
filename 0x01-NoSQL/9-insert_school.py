16e42e9dd97e17a54406c26a10c2d34a6aa32e8f
Eugene <140322568+yoojen@users.noreply.github.com>
2023-12-19 17:41:50 +0200

    replace insert_one with insert

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-    """Insert new data in school collection"""
 
 
 def insert_school(mongo_collection, **kwargs):
     """Insert new data in school collection"""
-    return mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert(kwargs)
